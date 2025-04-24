b07ca720aa7f143091f6998e9ab339922894bfa1
yuyuranium <n28111534@gs.ncku.edu.tw>
2025-04-24 12:49:33 +0800

    Don't use shallow LLVM flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
     flake-utils.url = "github:numtide/flake-utils";
-    formosa-llvm.url = "git+ssh://git@git.caslab.ee.ncku.edu.tw/formosa-gpgpu/formosa-llvm.git?shallow=1";
+    formosa-llvm.url = "git+ssh://git@git.caslab.ee.ncku.edu.tw/formosa-gpgpu/formosa-llvm.git";
   };
 
   outputs = { self, nixpkgs, flake-utils, formosa-llvm }:
